fb8340cee5d8b7cdb75b7f1b474e26a33b5cfa32
Reinaldo de Souza Jr <juniorz@gmail.com>
2016-08-22 12:12:51 -0500

    Decouple from the server

--- gui/client.go
+++ gui/client.go
@@ -4,8 +4,9 @@ import (
 	"log"
 	"net"
 	"net/rpc"
+	"net/rpc/jsonrpc"
 
-	"github.com/twstrike/nyms-agent/protocol"
+	"github.com/twstrike/nyms-agent/protocol/types"
 )
 
 type client struct {
@@ -17,23 +18,24 @@ func (c *client) establishConn() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	c.rpc = protocol.NewClient(conn)
+
+	c.rpc = jsonrpc.NewClient(conn)
 }
 
-func (c *client) pubKeyRing() protocol.GetKeyRingResult {
+func (c *client) pubKeyRing() types.GetKeyRingResult {
 	c.establishConn()
-	var pubKeyRing protocol.GetKeyRingResult
-	err := c.rpc.Call("Protocol.GetPublicKeyRing", protocol.VoidArg{}, &pubKeyRing)
+	var pubKeyRing types.GetKeyRingResult
+	err := c.rpc.Call("Protocol.GetPublicKeyRing", types.VoidArg{}, &pubKeyRing)
 	if err != nil {
 		log.Fatal("GetPublicKeyRing error:", err)
 	}
 	return pubKeyRing
 }
 
-func (c *client) createKeyPair(name, email, comment string) protocol.GetKeyInfoResult {
+func (c *client) createKeyPair(name, email, comment string) types.GetKeyInfoResult {
 	c.establishConn()
-	var keyinfo protocol.GetKeyInfoResult
-	err := c.rpc.Call("Protocol.GenerateKeys", protocol.GenerateKeysArgs{name, email, comment, ""}, &keyinfo)
+	var keyinfo types.GetKeyInfoResult
+	err := c.rpc.Call("Protocol.GenerateKeys", types.GenerateKeysArgs{name, email, comment, ""}, &keyinfo)
 	if err != nil {
 		log.Fatal("GenerateKeys error:", err)
 	}
